577b80f2079a317d62b66316612526746a8348f6
Kenneth Lyons <ixjlyons@gmail.com>
2016-02-20 15:18:54 -0800

    Updated fish prompt. Added shortcut for git remote update.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,21 +1,17 @@
 function fish_prompt
     set last_status $status
 
-    set_color normal
-    printf '│'
     set_color blue
     printf '%s' (whoami)
     set_color cyan
     printf '::'
     set_color green
     printf '%s' (hostname)
-    set_color normal
-    printf '│ '
 
     set_color $fish_color_cwd
-    printf '(%s)' (prompt_pwd)
+    printf '[%s]' (prompt_pwd)
     set_color normal
     printf '%s ' (__fish_git_prompt)
     set_color normal
-    printf '\n└─▶ '
+    printf '\n〉'
 end
