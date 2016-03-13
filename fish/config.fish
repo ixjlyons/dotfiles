8a07d84e0edb4c4a7957e603bd54d3c2e1a488b3
Kenneth Lyons <ixjlyons@gmail.com>
2016-03-13 10:45:52 -0700

    Cleaned up fish config a bit.

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,7 @@
-set ANDROID_HOME /home/kenny/usr/android/android-sdk
-set ANDROID_NDK /home/kenny/usr/android/android-ndk
-
+set PATH $PATH ~/bin ~/.local/bin
+set -x VREP /home/kenny/usr/vrep/vrep
+set -x EDITOR vim
 set -x GOPATH /home/kenny/go
-
-set PATH $PATH ~/bin ~/.local/bin ~/.cabal/bin $ANDROID_HOME/platform-tools
-
-set fish_color_cwd white
 
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
@@ -14,8 +10,3 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_char_dirtystate '★'
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '←'
-
-set -x VREP /home/kenny/usr/vrep/vrep
-set -x EDITOR vim
-
-eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
