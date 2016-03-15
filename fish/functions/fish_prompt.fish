a47257fba51bd04f922851887aeb9fd81e4452f7
Kenneth Lyons <ixjlyons@gmail.com>
2016-03-15 11:44:47 -0700

    Fix fish prompt color.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,7 +8,7 @@ function fish_prompt
     set_color green
     printf '%s' (hostname)
 
-    set_color $fish_color_cwd
+    set_color normal
     printf '[%s]' (prompt_pwd)
     set_color normal
     printf '%s ' (__fish_git_prompt)
