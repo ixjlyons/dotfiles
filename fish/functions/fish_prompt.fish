a380f6609fcdebbd2a29efe8275af60830883b32
Kenneth Lyons <ixjlyons@gmail.com>
2016-03-25 14:10:20 -0700

    More efficient fish prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,17 +1,21 @@
 function fish_prompt
-    set last_status $status
+    set -l pwd $PWD
+    set -l home_dir (string match -r "$HOME(/.*|\$)" "$pwd")
+    if set -q home_dir[2]
+        set pwd "~$home_dir[2]"
+    end
 
     set_color blue
-    printf '%s' (whoami)
+    printf "%s" (whoami)
     set_color cyan
     printf '::'
     set_color green
-    printf '%s' (hostname)
+    printf "%s" (hostname)
 
     set_color normal
-    printf '[%s]' (prompt_pwd)
+    printf "[$pwd]"
     set_color normal
-    printf '%s ' (__fish_git_prompt)
+    printf "$__fish_git_prompt "
     set_color normal
     printf '\n〉'
 end
--- fish/functions/prompt_pwd.fish
+++
@@ -1,8 +0,0 @@
-# replaces '/home/$USER' with '~'
-function prompt_pwd
-    if test "$PWD" != "$HOME"
-        printf "%s" (echo $PWD | sed -e 's|/private||' -e "s|^$HOME|~|")
-    else
-        echo '~'
-    end
-end
